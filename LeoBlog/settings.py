1b13de2b362d7bb25c7df6ba5f93b36b29fde3f6
DewaSRY <sdewa6645@gmail.com>
2023-01-06 19:55:13 +0800

    uraaa

--- LeoBlog/settings.py
+++ LeoBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4y*^)h&3mt-b8xu#h@+^#b0p#p$$e!tfu_g%gxnalid%*!e279'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG =True 
+DEBUG =getenv('IS_DEVELOPMENT',True) 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [
+#     getenv('APP_HOST')
+# ]
 
 
 # Application definition
@@ -120,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT=BASE_DIR/'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR /"Static")
